5ff73019ebd1568027c06a4d21d964eebfebd4bf
Eliud Michira <eliudmichira7@gmail.com>
2025-03-27 03:54:20 +0300

    Fix GitHub Pages deployment configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Admin from './pages/Admin';
 import Voter from './pages/Voter';
 import Home from './pages/Home';
 
 function App() {
-  // Get the base URL for GitHub Pages
-  const basename = process.env.PUBLIC_URL;
-
   return (
-    <Router basename={basename}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
